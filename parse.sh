5032177709dff682b7e9bc6237751e2101ce9bdc
myabandeh <myabandeh@fb.com>
2019-08-30 14:25:34 -0700

    Add a readwhilewriting benchmark

--- parse.sh
+++ parse.sh
@@ -1,6 +1,6 @@
-base=expr
-output=output.txt
-echo -e "conf\tfill rMB/s\tfill wMB/s\tfill cpu/q\tread rMB/s\tread wMB/s\tread cpu/q" > output.txt
+base=exprtmp
+output=output-tmp.txt
+echo -e "conf\tfill rMB/s\tfill wMB/s\tfill ops\tfill cpu/q\tread rMB/s\tread wMB/s\tread ops\tread cpu/q\ttran rMB/s\ttran wMB/s\ttran ops\ttran cpu/q" > $output
 
 for d in `ls $base`; do
 ls $base/$d/conf.txt 2>/dev/null 1>/dev/null
@@ -11,11 +11,18 @@ conf=`cat $base/$d/conf.txt`
 fillr=`cat $base/$d/hw.adaptive | grep fill | head -1 | awk '{print $4}'`
 fillw=`cat $base/$d/hw.adaptive | grep fill | head -1 | awk '{print $6}'`
 fillcpu=`cat $base/$d/hw.adaptive | grep fill | tail -1 | awk '{print $6}'`
+fillops=`cat $base/$d/ow.adaptive | grep fillrandom | awk {'print $7}'`
 
 readr=`cat $base/$d/hw.adaptive | grep read | head -1 | awk '{print $4}'`
 readw=`cat $base/$d/hw.adaptive | grep read | head -1 | awk '{print $6}'`
 readcpu=`cat $base/$d/hw.adaptive | grep read | tail -1 | awk '{print $6}'`
+readops=`cat $base/$d/or.adaptive | grep readrandom | awk {'print $7}'`
 
-echo -e "$conf\t$fillr\t$fillw\t$fillcpu\t$readr\t$readw\t$readcpu" >> output.txt
+tranr=`cat $base/$d/hw.adaptive | grep tran | head -1 | awk '{print $4}'`
+tranw=`cat $base/$d/hw.adaptive | grep tran | head -1 | awk '{print $6}'`
+trancpu=`cat $base/$d/hw.adaptive | grep tran | tail -1 | awk '{print $6}'`
+tranops=`cat $base/$d/ot.adaptive | grep readwhilewriting | awk {'print $7}'`
+
+echo -e "$conf\t$fillr\t$fillw\t$fillops\t$fillcpu\t$tranr\t$tranw\t$tranops\t$trancpu\t$readr\t$readw\t$readops\t$readcpu" >> $output
 
 done
--- runandmeasure.sh
+++ runandmeasure.sh
@@ -32,19 +32,34 @@ kill $vpid
 kill $ipid
 du -hs $dbdir > dw.$sfx
 
+# Let the ongoing compactions to finish
 echo reading the db at `date` ...
 vmstat 1 >& vr.$sfx &
 vpid=$!
 iostat -kx 1 >& ir.$sfx &
 ipid=$!
-$bin --benchmarks="readrandom,stats" --use_existing_db=1 --db=$dbdir --duration=30 $defargs $myargs >& or.$sfx
+$bin --benchmarks="readrandom,stats" --use_existing_db=1 --db=$dbdir --duration=60 $defargs $myargs >& or.$sfx
 kill $vpid
 kill $ipid
 du -hs $dbdir > dr.$sfx
+
+# measuring performance under a realistice read-write workload
+echo transacting the db at `date` ...
+vmstat 1 >& vt.$sfx &
+vpid=$!
+iostat -kx 1 >& it.$sfx &
+ipid=$!
+$bin --benchmarks="readwhilewriting,stats" --use_existing_db=1 --db=$dbdir --duration=120 $defargs $myargs >& ot.$sfx
+kill $vpid
+kill $ipid
+du -hs $dbdir > dt.$sfx
+
 echo ...done at `date`
+
 # Sample line:
 # readrandom   :     104.110 micros/op 9605 ops/sec;  142.3 MB/s (21914 of 288999 found)
 nr=`grep readrandom or.$sfx | awk '{print $(NF-1)}'`
+nt=`grep readwhilewriting ot.$sfx | awk '{print $(NF-1)}'`
 
 echo iostat metrics > hw.$sfx
 printf "Stage\tNsamp\tr/s\trMB/s\tw/s\twMB/s\trGB\twGB\tr/i\tw/i\trkb/i\twkb/i\tMrows\n" >> hw.$sfx
@@ -54,9 +69,13 @@ grep -a $dname iw.$sfx | tail -$(( $c - 1 )) > tmp.iw
 c=$( grep -a $dname ir.$sfx | wc -l )
 grep -a $dname ir.$sfx | tail -$(( $c - 1 )) > tmp.ir
 
+c=$( grep -a $dname it.$sfx | wc -l )
+grep -a $dname it.$sfx | tail -$(( $c - 1 )) > tmp.it
+
 cat tmp.iw | awk '{ rs += $4; ws += $5; rkb += $6; wkb += $7; c += 1 } END { printf "fill\t%s\t%.0f\t%.1f\t%.0f\t%.1f\t%.1f\t%.1f\t%.5f\t%.5f\t%.5f\t%.5f\t%.1f\n", c, rs/c, rkb/1024.0/c, ws/c, wkb/1024.0/c, rkb/(1024*1024.0), wkb/(1024*1024.0), rs/c/q, rkb/c/q, ws/c/q, wkb/c/q, q/1000000.0 }' q=$nw  >> hw.$sfx
 cat tmp.ir | awk '{ rs += $4; ws += $5; rkb += $6; wkb += $7; c += 1 } END { printf "read\t%s\t%.0f\t%.1f\t%.0f\t%.1f\t%.1f\t%.1f\t%.5f\t%.5f\t%.5f\t%.5f\t%.1f\n", c, rs/c, rkb/1024.0/c, ws/c, wkb/1024.0/c, rkb/(1024*1024.0), wkb/(1024*1024.0), rs/c/q, rkb/c/q, ws/c/q, wkb/c/q, q/1000000.0 }' q=$nr  >> hw.$sfx
-cat tmp.iw tmp.ir | awk '{ rs += $4; ws += $5; rkb += $6; wkb += $7; c += 1 } END { printf "totl\t%s\t%.0f\t%.1f\t%.0f\t%.1f\t%.1f\t%.1f\t%.5f\t%.5f\t%.5f\t%.5f\t%.1f\n", c, rs/c, rkb/1024.0/c, ws/c, wkb/1024.0/c, rkb/(1024*1024.0), wkb/(1024*1024.0), rs/c/q, rkb/c/q, ws/c/q, wkb/c/q, q/1000000.0 }' q=$((nw+nr))  >> hw.$sfx
+cat tmp.it | awk '{ rs += $4; ws += $5; rkb += $6; wkb += $7; c += 1 } END { printf "tran\t%s\t%.0f\t%.1f\t%.0f\t%.1f\t%.1f\t%.1f\t%.5f\t%.5f\t%.5f\t%.5f\t%.1f\n", c, rs/c, rkb/1024.0/c, ws/c, wkb/1024.0/c, rkb/(1024*1024.0), wkb/(1024*1024.0), rs/c/q, rkb/c/q, ws/c/q, wkb/c/q, q/1000000.0 }' q=$nt  >> hw.$sfx
+cat tmp.iw tmp.ir tmp.it | awk '{ rs += $4; ws += $5; rkb += $6; wkb += $7; c += 1 } END { printf "totl\t%s\t%.0f\t%.1f\t%.0f\t%.1f\t%.1f\t%.1f\t%.5f\t%.5f\t%.5f\t%.5f\t%.1f\n", c, rs/c, rkb/1024.0/c, ws/c, wkb/1024.0/c, rkb/(1024*1024.0), wkb/(1024*1024.0), rs/c/q, rkb/c/q, ws/c/q, wkb/c/q, q/1000000.0 }' q=$((nw+nr+nt))  >> hw.$sfx
 
 c=$( grep -av swpd vw.$sfx | wc -l )
 grep -av swpd vw.$sfx | tail -$(( $c - 1 )) > tmp.vw
@@ -64,12 +83,16 @@ grep -av swpd vw.$sfx | tail -$(( $c - 1 )) > tmp.vw
 c=$( grep -av swpd vr.$sfx | wc -l )
 grep -av swpd vr.$sfx | tail -$(( $c - 1 )) > tmp.vr
 
+c=$( grep -av swpd vt.$sfx | wc -l )
+grep -av swpd vt.$sfx | tail -$(( $c - 1 )) > tmp.vt
+
 echo >> hw.$sfx
 echo vmstat metrics >> hw.$sfx
 printf "Stage\tsamp\tcs/s\tcpu/c\tcs/q\tcpu/q\n" >> hw.$sfx
 cat tmp.vw        | awk '{ cs += $12; cpu += $13 + $14; c += 1 } END { printf "fill\t%s\t%.0f\t%.1f\t%.3f\t%.6f\n", c, cs/c, cpu/c, cs/c/q, cpu/c/q }' q=$nw>> hw.$sfx
 cat        tmp.vr | awk '{ cs += $12; cpu += $13 + $14; c += 1 } END { printf "read\t%s\t%.0f\t%.1f\t%.3f\t%.6f\n", c, cs/c, cpu/c, cs/c/q, cpu/c/q }' q=$nr>> hw.$sfx
-cat tmp.vw tmp.vr | awk '{ cs += $12; cpu += $13 + $14; c += 1 } END { printf "totl\t%s\t%.0f\t%.1f\t%.3f\t%.6f\n", c, cs/c, cpu/c, cs/c/q, cpu/c/q }' q=$((nw+nr))>> hw.$sfx
+cat        tmp.vt | awk '{ cs += $12; cpu += $13 + $14; c += 1 } END { printf "tran\t%s\t%.0f\t%.1f\t%.3f\t%.6f\n", c, cs/c, cpu/c, cs/c/q, cpu/c/q }' q=$nt>> hw.$sfx
+cat tmp.vw tmp.vr tmp.vt | awk '{ cs += $12; cpu += $13 + $14; c += 1 } END { printf "totl\t%s\t%.0f\t%.1f\t%.3f\t%.6f\n", c, cs/c, cpu/c, cs/c/q, cpu/c/q }' q=$((nw+nr+nt))>> hw.$sfx
 
 w1=$( grep -a "^Cumulative compaction" ow.$sfx | tail -1 | awk '{ print $3 }' )
 r1=$( grep -a "^Cumulative compaction" ow.$sfx | tail -1 | awk '{ print $9 }' )
@@ -77,7 +100,10 @@ r1=$( grep -a "^Cumulative compaction" ow.$sfx | tail -1 | awk '{ print $9 }' )
 w2=$( grep "^Cumulative compaction" or.$sfx | tail -1 | awk '{ print $3 }' )
 r2=$( grep "^Cumulative compaction" or.$sfx | tail -1 | awk '{ print $9 }' )
 
-wt=$( echo "$w1 + $w2" | bc )
-rt=$( echo "$r1 + $r2" | bc )
-echo "w1 w2 r1 r2 :: wt rt" >> hw.$sfx
-echo $w1 $w2 $r1 $r2 :: $wt $rt >> hw.$sfx
+w3=$( grep "^Cumulative compaction" ot.$sfx | tail -1 | awk '{ print $3 }' )
+r3=$( grep "^Cumulative compaction" ot.$sfx | tail -1 | awk '{ print $9 }' )
+
+wt=$( echo "$w1 + $w2 + $w3" | bc )
+rt=$( echo "$r1 + $r2 + $r3" | bc )
+echo "w1 w2 w3 r1 r2 r3 :: wt rt" >> hw.$sfx
+echo $w1 $w2 $w3 $r1 $r2 $r3 :: $wt $rt >> hw.$sfx
